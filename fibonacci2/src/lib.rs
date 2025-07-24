64783a81fdc7196b23aa377e20a90fab4f77315e
melalj <mohammedelalj@outlook.com>
2025-07-24 14:10:56 +0100

    fibonaché :)

---
+++ fibonacci2/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 2..=n {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    b
+}
