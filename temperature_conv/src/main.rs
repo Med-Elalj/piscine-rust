46af5447ec9fcd886dbfc7c1641310ab8f592141
melalj <mohammedelalj@outlook.com>
2025-07-24 13:11:06 +0100

    hehe

--- temperature_conv/src/main.rs
+++ temperature_conv/src/main.rs
@@ -3,4 +3,5 @@ use temperature_conv::*;
 fn main() {
     println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
     println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
+    println!("{} C = {} F", 20.0, fahrenheit_to_celsius(20.0));
 }
