6fb27cf0a0cc2172d0b1ae569e989b885fd50c37
melalj <mohammedelalj@outlook.com>
2025-07-24 13:23:41 +0100

    tmp

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,11 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    round_to_two_decimals((f - 32.0) * (5.0 / 9.0))
+  (f - 32.0) * 1.0 / (9.0/5.0)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    round_to_two_decimals((c * (9.0 / 5.0)) + 32.0)
-}
-
-fn round_to_two_decimals(value: f64) -> f64 {
-    (value * 100.0).round() / 100.0
+  (9.0/5.0) * c + 32.0
 }
