79c05e5661109c4c251101e6745d761bc2530381
melalj <mohammedelalj@outlook.com>
2025-07-26 22:36:09 +0100

    arrays

--- arrays/src/lib.rs
+++ arrays/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn sum(a: &[i32]) -> i32 {
+    a.iter().sum()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn thirtytwo_tens() -> [i32; 32] {
+    [10;32]
 }
