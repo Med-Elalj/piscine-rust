0c356c3414d7199b13d25a4edd908b669ab18220
melalj <mohammedelalj@outlook.com>
2025-07-26 18:28:07 +0100

    borrow_me_the_reference

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn delete_and_backspace(s: &mut String) {
+    let mut result = String::new();
+    let mut i = 0;
+    let mut eat:usize = 0;
+
+    let chars: Vec<char> = s.chars().collect();
+
+    while i < chars.len() {
+        match chars[i] {
+            '-' => {
+                if !result.is_empty() {
+                    result.pop(); // Remove previous character
+                }
+            }
+            '+' => {
+                eat +=1        
+            }
+            c => {
+                if eat == 0 {
+                    result.push(c);
+                } else {
+                    eat-=1
+                }
+            }
+        }
+        i += 1;
+    }
+
+    *s = result;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn do_operations(v: &mut [String]) {
+    for s in v.iter_mut() {
+        if let Some(index) = s.find(|c| c == '+' || c == '-') {
+        let left = &s[..index];
+        let right = &s[index+1..];
+        let operator = &s[index..=index];
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        if let (Ok(a), Ok(b)) = (left.trim().parse::<i32>(), right.trim().parse::<i32>()) {
+            if operator == "+" {
+                *s = (a + b).to_string();
+            } else {
+                *s = (a - b).to_string();
+            }
+        }
     }
+}
 }
