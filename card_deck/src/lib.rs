d9894141f0a5ca928bdf1fad34285d77c92ad2e6
melalj <mohammedelalj@outlook.com>
2025-07-26 21:41:15 +0100

    card deck

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,14 +1,62 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use rand::Rng;
+
+#[derive(PartialEq)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(PartialEq)]
+pub enum Rank {
+    Ace,
+    Number(u8),
+    King,
+    Queen,
+    Jack,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Suit {
+    pub fn random() -> Suit {
+        let value = rand::rng().random_range(1..=4);//.gen_range(1..=4);
+        Suit::translate(value)
     }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!("Invalid value for Suit: {}", value),
+        }
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        let value = rand::rng().random_range(1..=13);
+        Rank::translate(value)
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => panic!("Invalid value for Rank: {}", value),
+        }
+    }
+}
+
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    card.rank == Rank::Ace || card.suit == Suit::Spade
 }
