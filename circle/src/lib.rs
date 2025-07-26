7585c69957452b96762bb681ce1b4b4c05f8897d
melalj <mohammedelalj@outlook.com>
2025-07-26 20:28:33 +0100

    circle : math man

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Clone, Copy)]
+pub struct Circle {
+	pub center: Point,
+	pub radius: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Circle {
+    pub fn new(x:f64,y:f64,r :f64) -> Circle {
+        Circle { center: Point(x, y), radius: r }
     }
+    pub fn diameter(self) -> f64 {
+        self.radius * 2.0
+    }
+    pub fn area(self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+    pub fn intersect(self, c: Circle) ->bool {
+        self.center.distance(c.center) <= self.radius+c.radius
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Point(pub f64,pub f64);
+
+impl Point {
+    pub fn distance(self: Point,to:Point)-> f64 {
+        ((self.0 - to.0).powi(2)+ (self.1 - to.1).powi(2)).sqrt()
+    }
+
 }
