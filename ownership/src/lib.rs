ca25a3cb1821c6fd131ccc6b5a59b6919891f721
melalj <mohammedelalj@outlook.com>
2025-07-26 16:43:04 +0100

    ownership

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn first_subword(mut s: String) -> String {
+    if let Some(pos) = s.find('_') {
+        s.truncate(pos);
+    } else {
+        let bytes = s.as_bytes();
+        for (i, &b) in bytes.iter().enumerate().skip(1) {
+            if b.is_ascii_uppercase() {
+                s.truncate(i);
+                break;
+            }
+        }
     }
+    s
 }
