96a253d437a34fe94c232828a9963c0489c386d4
melalj <mohammedelalj@outlook.com>
2025-07-27 16:50:08 +0100

    bigger

--- bigger/src/lib.rs
+++ bigger/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::{collections::HashMap, i32};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    let mut last = i32::MIN;
+    for ele in h {
+        last = if last >= ele.1 {last} else {ele.1}
     }
+    last
 }
