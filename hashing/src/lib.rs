464b764a62cc55bb6af6c666708b311bd3dc7adc
melalj <mohammedelalj@outlook.com>
2025-07-27 17:34:14 +0100

    please work

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+
+pub fn mean(list: &[i32]) -> f64 {
+    let mut res:f64 = 0.0;
+    for ele in list {
+        res += *ele as f64
+    }
+    res/= list.len() as f64;
+    res
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn median(list: &[i32]) -> i32 {
+    let mut x = list.to_vec();
+    x.sort();
+    if x.len()%2==1 {
+        x[x.len()/2]
+    }else {
+        ( x[x.len()/2] + x[(x.len()/2)+1] )/2
     }
+}
+
+pub fn mode(list: &[i32]) -> i32 {
+    let mut map: HashMap<i32,i32> = HashMap::with_capacity(list.len());
+    let mut last = i32::MIN;
+    let mut v = i32::MIN;
+    for ele in list {
+        *( map.entry(*ele).or_insert(0))+=1;
+    }
+    // println!("{:?}",map);
+    for ele in map {
+        v = if v >= ele.1 {v} else {last = ele.0; ele.1}
+    }
+    last
 }
