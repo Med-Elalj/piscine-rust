995e0a158ea39aac02d1d0e62669350876365e96
melalj <mohammedelalj@outlook.com>
2025-07-27 17:49:34 +0100

    how about now??

--- collect/src/lib.rs
+++ collect/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn bubble_sort(arr: &mut [i32]) {
     for _ in 0..arr.len() {
         for i in 1..arr.len() {
-            if arr[i] > arr[i - 1] {
+            if arr[i] < arr[i - 1] {
                 arr.swap(i, i - 1);
             }
         }
