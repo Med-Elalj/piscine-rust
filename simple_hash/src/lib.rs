49485c4b1f2494bcac0cfa992002dc3c5167772e
melalj <mohammedelalj@outlook.com>
2025-07-27 16:43:32 +0100

    hehe

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+
+/*
+Create a function named word_frequency_counter which will receive a vector of strings (each string being a single word) and return an HashMap with the word as the key and the number of repetitions as the value.
+
+Also create a function named nb_distinct_words which will take a reference to the HashMap and return the number of distinct words present in it.
+
+all the words tested will be lowercase
+*/
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut map: HashMap<&str, usize>=  HashMap::with_capacity(words.len());
+    for &ele in words.iter() {
+        *( map.entry(ele).or_insert(0_usize))+=1;
+    }
+    map
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    frequency_count.len()
 }
