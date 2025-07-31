aefde1acc179492ca036396b62f0aadf7946568c
melalj <mohammedelalj@outlook.com>
2025-07-31 19:35:24 +0100

    question_mark

--- question_mark/src/main.rs
+++ question_mark/src/main.rs
@@ -1,3 +1,15 @@
+use question_mark::*;
+
 fn main() {
-    println!("Hello, world!");
+    let a = One {
+        first_layer: Some(Two {
+            second_layer: Some(Three {
+                third_layer: Some(Four {
+                    fourth_layer: Some(1000)
+                })
+            })
+        })
+    };
+
+    println!("{:?}", a.get_fourth_layer());
 }
