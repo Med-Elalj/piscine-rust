779d26d98f4e990c8e574c9af2ec003ea7f9ec2a
melalj <mohammedelalj@outlook.com>
2025-08-03 17:11:29 +0100

    ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn num_to_ordinal(x: u32) -> String {
     let mut x1 = x.to_string();
-    match x % 10_u32 {
+    match if x > 20 {x % 10_u32} else {x} {
         1=> x1.push_str("st"),
         2=> x1.push_str("nd"),
         3=> x1.push_str("rd"),
