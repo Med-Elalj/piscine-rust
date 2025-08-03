7e1e0499c55d6c18d0ecf21f1ae95e259fb6ee92
melalj <mohammedelalj@outlook.com>
2025-08-03 16:50:27 +0100

    score

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn score(val: &str) -> u64 {
+    val.to_string().chars().fold(0, |score, c| {
+        score + match c.to_ascii_uppercase() {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 0,
+            _ => 0,
+        }
+    })
 }
