7bb44bfdeb7e4e4f85e8150824b4a3cdf3db6a59
melalj <mohammedelalj@outlook.com>
2025-08-03 18:30:21 +0100

    rot

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub fn rotate(input: &str, key: i8) -> String {
+    let key = if key > 0 {
+        (key % 26) as u8
+    } else  {
+        26 - ( -key % 26) as u8
+    };
+    String::from_utf8(input.to_string().bytes().map(|c| {
+        if c.is_ascii_uppercase() {
+            b'A'+ ((c-b'A' + key ) % 26) 
+        } else if c.is_ascii_lowercase() {
+            b'a'+ ((c-b'a' + key ) % 26) 
+        } else {
+            c
+        }
+    }).collect::<Vec<u8>>()).expect("well shit")
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
