719cf77491f5caece23c8fe33d978b03264cfeaa
melalj <mohammedelalj@outlook.com>
2025-08-03 19:56:18 +0100

    scytale_cipher

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,16 +1,12 @@
 pub fn scytale_cipher(message: &str, i: usize) -> String {
     let mut res = String::with_capacity(message.len());
-    // message.push_str(" ".repeat(i - ma));
-    let message = message.chars().collect::<Vec<char>>();
-    let rows = (message.len() + i - 1) / i;
+    let mut message = message.chars().collect::<Vec<char>>();
+    while message.len()% i != 0 {
+        message.push(' ');
+    }
     for idx in 0.. i {
-        for x in 0..rows {
-            let index = x * i + idx ;
-            if index < message.len() {
-                res.push(message[index]);
-            } else {
-                res.push(' ');
-            }
+        for x in 0..(message.len()/i) {
+            res.push(message[x*i+idx]);
         };
     };
     res
