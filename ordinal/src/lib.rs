1420b889915f17347080b2f1de2ef432a0a83a41
melalj <mohammedelalj@outlook.com>
2025-08-03 17:14:19 +0100

    fix

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn num_to_ordinal(x: u32) -> String {
     let mut x1 = x.to_string();
-    match if x > 20 {x % 10_u32} else {x} {
+    match if !(11..=13).contains(&(x % 100)) {x % 10_u32} else {x} {
         1=> x1.push_str("st"),
         2=> x1.push_str("nd"),
         3=> x1.push_str("rd"),
