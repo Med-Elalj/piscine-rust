ec12e68232ee4772eebd5b1f7fb8805783624547
melalj <mohammedelalj@outlook.com>
2025-08-09 18:48:35 +0100

    hehe

--- borrow_box/src/lib.rs
+++ borrow_box/src/lib.rs
@@ -29,7 +29,7 @@ impl GameSession {
 
     pub fn update_score(&mut self, user_name: &str) {
         if !self.read_winner().is_some() 
-        || self.p1.1 + self.p2.1 < self.nb_games / 2
+        || self.p1.1 + self.p2.1 <= self.nb_games / 2 + 1
         {
             if self.p1.0 == user_name {
                 self.p1.1 += 1;
