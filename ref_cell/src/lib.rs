0af115b260a0c62d6fc40b39910cc5dc6187c320
melalj <mohammedelalj@outlook.com>
2025-08-09 20:53:42 +0100

    test

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct Tracker {
+    pub messages: Rc<RefCell<Vec<String>>>,
+    value: usize,
+    max: usize,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Tracker {
+    pub fn new(max: usize) -> Self {
+        Self {
+            messages: Rc::new(RefCell::new(Vec::new())),
+            value: 0,
+            max,
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn set_value<T>(&mut self, val: &Rc<T>) {
+        let count = Rc::strong_count(val);
+        if count > self.max {
+            self.messages
+                .borrow_mut()
+                .push("Error: You can't go over your quota!".to_string());
+        } else {
+            self.value = count;
+            let percent = (self.value * 100) / self.max;
+            if percent > 70 {
+                self.messages.borrow_mut().push(format!(
+                    "Warning: You have used up over {}% of your quota!",
+                    percent
+                ));
+            }
+        }
+    }
+
+    pub fn peek<T>(&self, val: &Rc<T>) {
+        let count = Rc::strong_count(val);
+        let percent = (count * 100) / self.max;
+        self.messages.borrow_mut().push(format!(
+            "Info: This value would use {}% of your quota",
+            percent
+        ));
     }
 }
