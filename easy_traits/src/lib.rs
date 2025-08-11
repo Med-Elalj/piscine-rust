e9ee96b0d745c14e82162f29b15bf55a1b4a83dd
melalj <mohammedelalj@outlook.com>
2025-08-11 18:09:59 +0100

    easy_traits

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Clone)]
+pub struct StringValue {
+    pub value: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait AppendStr {
+    fn append_str(&mut self, str_to_append: String) -> Self;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn append_number(&mut self, nb_to_append: f64) -> Self;
+
+    fn remove_punctuation_marks(&mut self) -> Self;
+}
+
+impl AppendStr for StringValue {
+    fn append_str(&mut self, str_to_append: String) -> Self {
+        self.value.push_str(&str_to_append);
+        self.clone()
+    }
+
+    fn append_number(&mut self, nb_to_append: f64) -> Self {
+        // Convert number to string and append
+        self.value.push_str(&nb_to_append.to_string());
+        self.clone()
+    }
+
+    fn remove_punctuation_marks(&mut self) -> Self {
+        // Remove '.', ',', '?', '!' characters
+        self.value = self
+            .value
+            .chars()
+            .filter(|&c| c != '.' && c != ',' && c != '?' && c != '!')
+            .collect();
+        self.clone()
     }
 }
