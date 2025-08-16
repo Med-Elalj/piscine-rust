8fd69b794d5ac3f6a560dfb82541e5263de3cf85
melalj <mohammedelalj@outlook.com>
2025-08-16 16:18:36 +0100

    matrix

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -1,14 +1,57 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+// Scalar trait must be implemented for types used in the Matrix.
+// It must define associated functions zero() and one().
+pub trait Scalar {
+    fn zero() -> Self;
+    fn one() -> Self;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Scalar for i32 {
+    fn zero() -> Self {
+        0
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn one() -> Self {
+        1
+    }
+}
+
+impl Scalar for u32 {
+    fn zero() -> Self {
+        0
+    }
+
+    fn one() -> Self {
+        1
+    }
+}
+
+impl Scalar for f64 {
+    fn zero() -> Self {
+        0.0
+    }
+
+    fn one() -> Self {
+        1.0
+    }
+}
+
+impl<T: Scalar + Clone> Matrix<T> {
+    pub fn new() -> Matrix<T> {
+        Matrix(vec![vec![T::zero()]])
+    }
+
+    pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        Matrix(vec![vec![T::zero(); col]; row])
+    }
+
+    pub fn identity(n: usize) -> Matrix<T> {
+        let mut data = vec![vec![T::zero(); n]; n];
+        for i in 0..n {
+            data[i][i] = T::one();
+        }
+        Matrix(data)
     }
 }
