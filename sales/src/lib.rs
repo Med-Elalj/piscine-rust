7d3d7cbbfcf702a930e83ebaff7d32cb8849aa0e
melalj <mohammedelalj@outlook.com>
2025-08-18 16:08:34 +0100

    sales

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Clone, PartialEq)]
+pub struct Store {
+    pub products: Vec<(String, f32)>,
+}
+impl Store {
+    pub fn new(products: Vec<(String, f32)>) -> Store {
+        Store { products }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone, PartialEq)]
+pub struct Cart {
+    pub items: Vec<(String, f32)>,
+    pub receipt: Vec<f32>,
+}
+impl Cart {
+    pub fn new() -> Cart {
+        Cart {
+            items: Vec::new(),
+            receipt: Vec::new(),
+        }
+    }
+    pub fn insert_item(&mut self, s: &Store, ele: String) {
+        let w = s.products.iter().find(|(name, _)| name == &ele).unwrap();
+        self.items.push((w.0.clone(), w.1));
+    }
+    pub fn generate_receipt(&mut self) -> Vec<f32> {
+        for item in &self.items {
+            self.receipt.push(item.1);
+        }
+        self.receipt.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let discount = self.receipt.iter().cloned().fold(f32::INFINITY, f32::min);
+
+        let total: f32 = self.receipt.iter().sum();
+
+        self.receipt = self.receipt
+            .iter()
+            .map(|&price| {
+                let share = price / total;
+                ((price - (share * discount))* 100.0).round() / 100.0
+            })
+            .collect();
+        self.receipt.clone()
     }
 }
