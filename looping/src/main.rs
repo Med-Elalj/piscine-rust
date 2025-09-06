ed62e00ac4348539df2d198bf5d978d2c169c976
melalj <mohammedelalj@outlook.com>
2025-09-06 15:54:48 +0100

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle = "I am the beginning of the end, and the end of time and space. \
+I am essential to creation, and I surround every place. What am I?";
+    let right = "the letter e";
+    let mut x = 0;
+
+    loop {
+        println!("{}", puzzle);
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        let left = input.trim().to_lowercase();
+        x += 1;
+
+        if left == right {
+            println!("Number of trials: {}", x);
+            break;
+        }
+
+        if x >= 101 {
+            println!("faild more than 100 trials: {}", left);
+            break;
+        }
+    }
 }
