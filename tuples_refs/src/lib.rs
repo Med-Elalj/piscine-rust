a797e0e562a11360fd84bb0f659eef2d63514bef
melalj <mohammedelalj@outlook.com>
2025-09-06 16:06:30 +0100

    tuples_refs

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+
+pub fn first_name(student: &Student) -> &str {
+    &student.1
+}
+
+pub fn last_name(student: &Student) -> &str {
+    &student.2
+}
+pub struct Student(
+    pub u32,
+    pub String,
+    pub String
+);
