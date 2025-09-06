e5c0e129f6cc2a01eb7e3bee9ddc406696129557
melalj <mohammedelalj@outlook.com>
2025-09-06 16:32:31 +0100

    copy

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn nbr_function(c: i32) -> (i32, f64, f64) {
+    (c,(c as f64).exp(),(c.abs()as f64).ln())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn str_function(a: String) -> (String, String) {
+    (a.clone(),a
+        .split(' ')
+        .map(|s| {
+            s.parse::<f64>()
+                .map(|f: f64| f.exp().to_string())
+                .unwrap_or_else(|_| "0.0".to_string())
+        })
+        .collect::<Vec<String>>()
+        .join(" "))
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    (b.clone(),b.iter().map(|num| {(num.abs()as f64).ln()}).collect())
 }
