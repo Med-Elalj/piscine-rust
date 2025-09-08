87d3441b4ed4c7e61442fc8fa71705880849f191
melalj <mohammedelalj@outlook.com>
2025-09-08 13:56:04 +0100

    card-deck

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,14 +1,69 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Debug, PartialEq)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, PartialEq)]
+pub enum Rank {
+    Ace,
+    Number(u8),
+    King,
+    Queen,
+    Jack,
+}
+fn random() -> u64 {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    now.subsec_nanos() as u64 + now.as_secs()
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Suit {
+    pub fn random() -> Suit {
+        let value: u8 = (random() % 4+1) as u8;
+        Suit::translate(value as u8)
     }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!("Invalid value for Suit: {}", value),
+        }
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        let value: u8 = (random() % 13+1) as u8;
+        Rank::translate(value)
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => panic!("Invalid value for Rank: {}", value),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    card.rank == Rank::Ace && card.suit == Suit::Spade
 }
