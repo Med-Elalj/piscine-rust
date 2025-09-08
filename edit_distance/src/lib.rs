bcc30906556143e468a5a53fdb22f2c8f5c1770a
melalj <mohammedelalj@outlook.com>
2025-09-08 13:58:59 +0100

    edit-distance

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn edit_distance(source: &str, target: &str) -> usize {
+    let a: Vec<char>  = source.chars().collect();
+    let b: Vec<char>  = target.chars().collect();
+
+    let mut x = vec![vec![0_usize;b.len()+1];a.len()+1];
+    {
+        for i in 0..=a.len() {
+            x[i][0] = i
+        }
+    }
+    {
+        for i in 0..=b.len() {
+            x[0][i] = i
+        }
+    }
+
+    for i in 1..=a.len() {
+        for j in 1..=b.len() {
+            let v = if a[i - 1] == b[j - 1] { 0 } else { 1 };
+            x[i][j] = min(
+                x[i - 1][ j ] + 1,
+                x[ i ][j - 1] + 1,
+                x[i - 1][j - 1] + v
+            )
+        }
+    }
+
+    x[a.len()][b.len()]
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn min(a: usize,b: usize,c: usize) -> usize {
+    if c < b && c < a {
+        c
+    } else if b < a {
+        b
+    } else {
+        a
     }
 }
