bb82c713ede470d9d8685fc8f7d6e6779e84220a
melalj <mohammedelalj@outlook.com>
2025-09-08 14:04:05 +0100

    string-permutation

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn is_permutation(s1: &str, s2: &str) -> bool {
+    let mut m1: HashMap<char, u32> = HashMap::with_capacity(s1.len());
+    let mut m2: HashMap<char, u32> = HashMap::with_capacity(s2.len());
+    for e in s1.chars() {
+        *( m1.entry(e).or_insert(0))+=1;
     }
+    for e in s2.chars() {
+        *( m2.entry(e).or_insert(0))+=1;
+    }
+    for e in m1.clone() {
+    if let Some(c) = m2.get(&e.0) {
+        if *c == e.1 {
+            continue;
+        }
+    }
+    return false;
+    }
+    m1.len() == m2.len()
 }
