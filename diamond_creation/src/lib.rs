237664b4bb9944bf8aa26422be35c7b8ffc276b5
melalj <mohammedelalj@outlook.com>
2025-09-13 13:18:41 +0100

    diamond-creation

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub fn get_diamond(c: char) -> Vec<String> {
+    let start = 'A' as u8;
+    let end = c as u8;
+    let size = (end - start) as usize;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    let width = size * 2 + 1;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let mut lines:Vec<String> = Vec::with_capacity(width);
+
+    for i in 0..=size {
+        let letter = (start + i as u8) as char;
+
+        let mut line = vec![' '; width];
+
+        if i == 0 {
+            line[size] = letter;
+        } else {
+            line[size - i] = letter;
+            line[size + i] = letter;
+        }
+
+        lines.push(line.into_iter().collect());
     }
+    for i in (0..size).rev() {
+        lines.push(lines[i].clone());
+    }
+
+    lines
 }
