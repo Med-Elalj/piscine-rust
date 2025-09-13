3838ea0a7e30fc84962ad15be193f9bd1cf61db8
melalj <mohammedelalj@outlook.com>
2025-09-13 08:56:15 +0100

    error-types

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use chrono::Local;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    pub form_values: (&'static str, String),
+    pub date: String,
+    pub err: &'static str,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        let date = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        Self {
+            form_values: (field_name, field_value),
+            date,
+            err,
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    pub name: String,
+    pub password: String,
+}
+
+impl Form {
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name.is_empty() {
+            return Err(FormError::new("name", self.name.clone(), "Username is empty"));
+        }
+
+        if self.password.len() < 8 {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be at least 8 characters long",
+            ));
+        }
+        let has_letter = self.password.chars().any(|c| c.is_ascii_alphabetic());
+        let has_digit = self.password.chars().any(|c| c.is_ascii_digit());
+        let has_symbol = self.password.chars().any(|c| {
+            c.is_ascii_graphic() && !c.is_ascii_alphanumeric()
+        });
+
+        if !(has_letter && has_digit && has_symbol) {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be a combination of ASCII numbers, letters and symbols",
+            ));
+        }
+
+        Ok(())
     }
 }
