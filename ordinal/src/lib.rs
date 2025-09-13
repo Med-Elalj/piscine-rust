b3829fa18dca807417e29b25e490d7f9b1d5b004
melalj <mohammedelalj@outlook.com>
2025-09-13 12:28:56 +0100

    ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn num_to_ordinal(x: u32) -> String {
+    let mut x1 = x.to_string();
+    match if !(11..=13).contains(&(x % 100)) {x % 10_u32} else {x} {
+        1=> x1.push_str("st"),
+        2=> x1.push_str("nd"),
+        3=> x1.push_str("rd"),
+        _ => x1.push_str("th"),
     }
+    x1
 }
