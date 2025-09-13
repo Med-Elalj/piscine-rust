6f156d5944aa2adcda13fd75aeb552aad3afc87d
melalj <mohammedelalj@outlook.com>
2025-09-13 09:21:59 +0100

    macro-calculator

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub use json::JsonValue;
+
+pub struct Food {
+    pub name: String,
+    pub calories: (String, String),
+    pub fats: f64,
+    pub carbs: f64,
+    pub proteins: f64,
+    pub nbr_of_portions: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn calculate_macros(foods: &[Food]) -> json::JsonValue {
+    let mut tcrb = 0.0;
+    let mut tcal = 0.0;
+    let mut tp = 0.0;
+    let mut tf = 0.0;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for f in foods {
+        let kcal: f64 = f.calories.1.trim_end_matches("kcal").parse().unwrap_or(0.0);
+
+        tcal += kcal * f.nbr_of_portions;
+        tcrb += f.carbs * f.nbr_of_portions;
+        tp += f.proteins * f.nbr_of_portions;
+        tf += f.fats * f.nbr_of_portions;
+    }
+
+    fn round_and_format(value: f64) -> f64 {
+        let r = (value * 100.0).round() / 100.0;
+        if (r * 10.0) % 10.0 == 0.0 {
+            (r * 10.0).round() / 10.0
+        } else {
+            r
+        }
+    }
+
+    json::object! {
+        "cals" => round_and_format(tcal),
+        "carbs" => round_and_format(tcrb),
+        "proteins" => round_and_format(tp),
+        "fats" => round_and_format(tf),
     }
 }
