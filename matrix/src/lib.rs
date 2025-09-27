5721e8ee53ca05498b32543c9a947e5b1f9bc468
melalj <mohammedelalj@outlook.com>
2025-09-27 19:05:06 +0100

    matrix

---
+++ matrix/src/lib.rs
@@ -0,0 +1,57 @@
+#[derive(Debug, PartialEq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+// Scalar trait must be implemented for types used in the Matrix.
+// It must define associated functions zero() and one().
+pub trait Scalar {
+    fn zero() -> Self;
+    fn one() -> Self;
+}
+
+impl Scalar for i32 {
+    fn zero() -> Self {
+        0
+    }
+
+    fn one() -> Self {
+        1
+    }
+}
+
+impl Scalar for u32 {
+    fn zero() -> Self {
+        0
+    }
+
+    fn one() -> Self {
+        1
+    }
+}
+
+impl Scalar for f64 {
+    fn zero() -> Self {
+        0.0
+    }
+
+    fn one() -> Self {
+        1.0
+    }
+}
+
+impl<T: Scalar + Clone> Matrix<T> {
+    pub fn new() -> Matrix<T> {
+        Matrix(vec![vec![T::zero()]])
+    }
+
+    pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        Matrix(vec![vec![T::zero(); col]; row])
+    }
+
+    pub fn identity(n: usize) -> Matrix<T> {
+        let mut data = vec![vec![T::zero(); n]; n];
+        for i in 0..n {
+            data[i][i] = T::one();
+        }
+        Matrix(data)
+    }
+}
