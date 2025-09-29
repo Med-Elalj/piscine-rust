8b161078b5f842ffb8e431b9bec031cd88b6414b
melalj <mohammedelalj@outlook.com>
2025-09-29 19:03:28 +0100

    adding_twice

--- adding_twice/src/lib.rs
+++ adding_twice/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add_curry(x: i32) -> impl Fn(i32) -> i32 {
+    move |y| x + y
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn twice<F>(f: F) -> impl Fn(i32) -> i32
+where
+    F: Fn(i32) -> i32,
+{
+    move |x| f(f(x))
 }
