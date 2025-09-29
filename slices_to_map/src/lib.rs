f1a6ac9008a07a97e963ca962642ea6762b06c2d
melalj <mohammedelalj@outlook.com>
2025-09-29 19:06:46 +0100

    slices_to_map

--- slices_to_map/src/lib.rs
+++ slices_to_map/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn slices_to_map<'a, T: std::hash::Hash + Eq, U>(keys: &'a [T], values: &'a [U]) -> HashMap<&'a T, &'a U> {
+    let len = std::cmp::min(keys.len(), values.len());
+    let mut map = HashMap::with_capacity(len);
+    for i in 0..len {
+        map.insert(&keys[i], &values[i]);
     }
+    map
 }
